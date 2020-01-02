0b8a199a6440345796188323f083004a1b53dcc1
zmzmhhhxyj <zmzmhhhxyj@163.com>
2020-01-01 18:56:05 -0800

    finish assignment2

---
+++ assignment2/PyTorch.py
@@ -0,0 +1,80 @@
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
+
+import torchvision.datasets as dset
+import torchvision.transforms as T
+import torch.nn.functional as F 
+
+import numpy as np
+
+dtype = torch.float32 
+
+def flatten(x):
+    N = x.shape[0] # read in N, C, H, W
+    return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
+
+def three_layer_convnet(x, params):
+    """
+    Performs the forward pass of a three-layer convolutional network with the
+    architecture defined above.
+
+    Inputs:
+    - x: A PyTorch Tensor of shape (N, 3, H, W) giving a minibatch of images
+    - params: A list of PyTorch Tensors giving the weights and biases for the
+      network; should contain the following:
+      - conv_w1: PyTorch Tensor of shape (channel_1, 3, KH1, KW1) giving weights
+        for the first convolutional layer
+      - conv_b1: PyTorch Tensor of shape (channel_1,) giving biases for the first
+        convolutional layer
+      - conv_w2: PyTorch Tensor of shape (channel_2, channel_1, KH2, KW2) giving
+        weights for the second convolutional layer
+      - conv_b2: PyTorch Tensor of shape (channel_2,) giving biases for the second
+        convolutional layer
+      - fc_w: PyTorch Tensor giving weights for the fully-connected layer. Can you
+        figure out what the shape should be?
+      - fc_b: PyTorch Tensor giving biases for the fully-connected layer. Can you
+        figure out what the shape should be?
+    
+    Returns:
+    - scores: PyTorch Tensor of shape (N, C) giving classification scores for x
+    """
+    conv_w1, conv_b1, conv_w2, conv_b2, fc_w, fc_b = params
+    scores = None
+    ################################################################################
+    # TODO: Implement the forward pass for the three-layer ConvNet.                #
+    ################################################################################
+    # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    m = F.conv2d(x,conv_w1,conv_b1,padding=2)
+    m = F.relu(m)
+    m = F.conv2d(m,conv_w2,conv_b2,padding=1)
+    m = F.relu(m)
+    m = flatten(m)
+    scores = F.linear(m,fc_w.transpose(1,0),fc_b)
+
+
+
+    # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    ################################################################################
+    #                                 END OF YOUR CODE                             #
+    ################################################################################
+    return scores
+
+def three_layer_convnet_test():
+    x = torch.zeros((64, 3, 32, 32), dtype=dtype)  # minibatch size 64, image size [3, 32, 32]
+
+    conv_w1 = torch.zeros((6, 3, 5, 5), dtype=dtype)  # [out_channel, in_channel, kernel_H, kernel_W]
+    conv_b1 = torch.zeros((6,))  # out_channel
+    conv_w2 = torch.zeros((9, 6, 3, 3), dtype=dtype)  # [out_channel, in_channel, kernel_H, kernel_W]
+    conv_b2 = torch.zeros((9,))  # out_channel
+
+    # you must calculate the shape of the tensor after two conv layers, before the fully-connected layer
+    fc_w = torch.zeros((9 * 32 * 32, 10))
+    fc_b = torch.zeros(10)
+
+    scores = three_layer_convnet(x, [conv_w1, conv_b1, conv_w2, conv_b2, fc_w, fc_b])
+    print(scores.size())  # you should see [64, 10]
+three_layer_convnet_test()
